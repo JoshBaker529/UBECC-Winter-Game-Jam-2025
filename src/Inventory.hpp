365746a35721762661c9ea981b87703e42cb01a6
Josh Baker <JoshBaker529@Gmail.com>
2025-12-23 15:01:43 -0500

    Implemented barebones for adding items to inventory

--- src/Inventory.hpp
+++ src/Inventory.hpp
@@ -3,11 +3,16 @@
 
 #include "Item.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Keyboard.hpp"
 #include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <array>
+#include <iostream>
 
+////////////////////////////////////////////////////////////////////////////////
 // Defines, because magic numbers are bad :D
+////////////////////////////////////////////////////////////////////////////////
+
 // Size
 #define WIDTH 700.f
 #define HEIGHT 300.f
@@ -23,10 +28,18 @@
 // Colors!
 #define BACKGROUND_COLOR 0xF0F0F0FF
 #define SLOT_COLOR 0xBBBBBBFF
+#define TRANSPARENT 0x00000000
 
 // Textures!
 #define TEXTURE_WIDTH 100.f
 #define TEXTURE_HEIGHT 100.f
+
+// Insert/Remove
+enum AddRemove { ADD, REMOVE };
+
+////////////////////////////////////////////////////////////////////////////////
+// Class Definition
+////////////////////////////////////////////////////////////////////////////////
 
 class Inventory {
 private:
@@ -36,20 +49,67 @@ private:
   std::array<Item, SLOTS> inventory;
   std::array<bool, SLOTS> slot_filled;
 
+  void changeInventorySlot(int, AddRemove, Item = Item());
+  int VertexArrayIndexFromPosition(int);
+
 public:
   Inventory(sf::Vector2f);
   ~Inventory();
   bool isOpen();
   void toggleOpen();
   sf::VertexArray getArray();
+  void insertItem(int, Item);
 
-  void draw(sf::RenderWindow);
+  void draw(sf::RenderWindow &);
 };
+
+////////////////////////////////////////////////////////////////////////////////
+// Private Methods
+////////////////////////////////////////////////////////////////////////////////
+
+inline void Inventory::changeInventorySlot(int pos, AddRemove ar, Item item) {
+
+  std::cout << "Changing item at pos " << pos << "\n" << std::flush;
+  int starting_index = VertexArrayIndexFromPosition(pos);
+
+  // For now, just changing the color
+  // Will need to update texture coords later
+  switch (ar) {
+  case ADD:
+    std::cout << "Adding at pos " << pos << "\n" << std::flush;
+    array[starting_index + 0].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    array[starting_index + 1].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    array[starting_index + 2].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    array[starting_index + 3].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    array[starting_index + 4].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    array[starting_index + 5].color = sf::Color(SLOT_COLOR + 0xE0101000);
+    break;
+  case REMOVE:
+    array[starting_index + 0].color = sf::Color(TRANSPARENT);
+    array[starting_index + 1].color = sf::Color(TRANSPARENT);
+    array[starting_index + 2].color = sf::Color(TRANSPARENT);
+    array[starting_index + 3].color = sf::Color(TRANSPARENT);
+    array[starting_index + 4].color = sf::Color(TRANSPARENT);
+    array[starting_index + 5].color = sf::Color(TRANSPARENT);
+    break;
+  }
+}
+
+inline int Inventory::VertexArrayIndexFromPosition(int pos) {
+  int index_start = (SLOTS + 1) * 6;
+  int index = pos * 6 + index_start;
+  return index;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Public Methods
+////////////////////////////////////////////////////////////////////////////////
 
 inline Inventory::Inventory(sf::Vector2f vec) {
   start = vec;
 
   for (int i = 0; i < SLOTS; i++) {
+    slot_filled[i] = false;
   }
 
   // NOTE:
@@ -103,56 +163,71 @@ inline Inventory::Inventory(sf::Vector2f vec) {
     array[index + 3].position = DownRight;
     array[index + 4].position = DownLeft;
     array[index + 5].position = UpLeft;
+
     array[index + 0].color = sf::Color(SLOT_COLOR);
     array[index + 1].color = sf::Color(SLOT_COLOR);
     array[index + 2].color = sf::Color(SLOT_COLOR);
     array[index + 3].color = sf::Color(SLOT_COLOR);
     array[index + 4].color = sf::Color(SLOT_COLOR);
     array[index + 5].color = sf::Color(SLOT_COLOR);
+
     x += slot_width;
   }
 
   x = start.x;
   y = start.y;
   // Item Layer
-  // for (int slot = 0; slot < 21; slot++) {
-  //   if (slot % COLUMNS == 0 && slot != 0) {
-  //     x = start.x;
-  //     y = y + slot_height;
-  //   }
-  //
-  //   int index_start = (SLOTS + 1) * 6;
-  //   int index = slot * 6 + index_start;
-  //   sf::Vector2f UpLeft(x + border_width, y + boarder_height),
-  //       UpRight(x + slot_width - border_width, y + boarder_height),
-  //       DownLeft(x + border_width, y + slot_height - boarder_height),
-  //       DownRight(x + slot_width - border_width,
-  //                 y + slot_width - boarder_height);
-  //
-  //   array[index + 0].position = UpLeft;
-  //   array[index + 1].position = UpRight;
-  //   array[index + 2].position = DownRight;
-  //   array[index + 3].position = DownRight;
-  //   array[index + 4].position = DownLeft;
-  //   array[index + 5].position = UpLeft;
-  //
-  //   if (slot_filled[slot]) {
-  //     sf::Vector2f item_texture = inventory[slot].getTexturePosition();
-  //     sf::Vector2f TextureUpLeft(item_texture),
-  //         TextureUpRight(item_texture.x + TEXTURE_WIDTH, item_texture.y),
-  //         TextureDownLeft(item_texture.x, item_texture.y + TEXTURE_HEIGHT),
-  //         TextureDownRight(item_texture.x + TEXTURE_WIDTH,
-  //                          item_texture.y + TEXTURE_HEIGHT);
-  //
-  //     array[index + 0].texCoords = TextureUpLeft;
-  //     array[index + 1].texCoords = TextureUpRight;
-  //     array[index + 2].texCoords = TextureDownRight;
-  //     array[index + 3].texCoords = TextureDownRight;
-  //     array[index + 4].texCoords = TextureDownLeft;
-  //     array[index + 5].texCoords = TextureUpLeft;
-  //     x += slot_width;
-  //   }
-  // }
+  for (int slot = 0; slot < 21; slot++) {
+    if (slot % COLUMNS == 0 && slot != 0) {
+      x = start.x;
+      y = y + slot_height;
+    }
+
+    int index_start = (SLOTS + 1) * 6;
+    int index = slot * 6 + index_start;
+    sf::Vector2f UpLeft(x + border_width, y + boarder_height),
+        UpRight(x + slot_width - border_width, y + boarder_height),
+        DownLeft(x + border_width, y + slot_height - boarder_height),
+        DownRight(x + slot_width - border_width,
+                  y + slot_width - boarder_height);
+
+    array[index + 0].position = UpLeft;
+    array[index + 1].position = UpRight;
+    array[index + 2].position = DownRight;
+    array[index + 3].position = DownRight;
+    array[index + 4].position = DownLeft;
+    array[index + 5].position = UpLeft;
+
+    if (slot_filled[slot]) {
+      sf::Vector2f item_texture = inventory[slot].getTexturePosition();
+      sf::Vector2f TextureUpLeft(item_texture),
+          TextureUpRight(item_texture.x + TEXTURE_WIDTH, item_texture.y),
+          TextureDownLeft(item_texture.x, item_texture.y + TEXTURE_HEIGHT),
+          TextureDownRight(item_texture.x + TEXTURE_WIDTH,
+                           item_texture.y + TEXTURE_HEIGHT);
+
+      array[index + 0].texCoords = TextureUpLeft;
+      array[index + 1].texCoords = TextureUpRight;
+      array[index + 2].texCoords = TextureDownRight;
+      array[index + 3].texCoords = TextureDownRight;
+      array[index + 4].texCoords = TextureDownLeft;
+      array[index + 5].texCoords = TextureUpLeft;
+      array[index + 0].color = sf::Color(SLOT_COLOR + 0x30101000);
+      array[index + 1].color = sf::Color(SLOT_COLOR + 0x30101000);
+      array[index + 2].color = sf::Color(SLOT_COLOR + 0x30101000);
+      array[index + 3].color = sf::Color(SLOT_COLOR + 0x30101000);
+      array[index + 4].color = sf::Color(SLOT_COLOR + 0x30101000);
+      array[index + 5].color = sf::Color(SLOT_COLOR + 0x30101000);
+    } else {
+      array[index + 0].color = sf::Color(TRANSPARENT);
+      array[index + 1].color = sf::Color(TRANSPARENT);
+      array[index + 2].color = sf::Color(TRANSPARENT);
+      array[index + 3].color = sf::Color(TRANSPARENT);
+      array[index + 4].color = sf::Color(TRANSPARENT);
+      array[index + 5].color = sf::Color(TRANSPARENT);
+    }
+    x += slot_width;
+  }
 }
 
 inline Inventory::~Inventory() {
@@ -165,12 +240,17 @@ inline void Inventory::toggleOpen() { open ^= 1; }
 
 inline sf::VertexArray Inventory::getArray() { return array; }
 
-inline void Inventory::draw(sf::RenderWindow window) {
+inline void Inventory::draw(sf::RenderWindow &window) {
+  if (open) {
+    window.draw(array);
+  }
+}
 
-  Item item("Greg", sf::Vector2f{100, 100}, 1, CONSUMABLE);
-
-  inventory[1] = item;
-  slot_filled[1] = true;
-
-  window.draw(array);
+inline void Inventory::insertItem(int pos, Item item) {
+  if (pos < 0 || pos > SLOTS) {
+    return;
+  }
+  slot_filled[pos] = true;
+  inventory[pos] = item;
+  changeInventorySlot(pos, ADD, item);
 }
