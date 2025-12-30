a88d407b3eac0762c31ede71401724b6884ce78e
Josh Baker <JoshBaker529@Gmail.com>
2025-12-30 13:10:52 -0500

    Added a PrintStats debug function

--- src/Stats.hpp
+++ src/Stats.hpp
@@ -1,5 +1,6 @@
 // This class holds all the information regarding the player stats
 
+#include <iostream>
 class Stats {
 public:
   Stats();
@@ -8,6 +9,7 @@ public:
   Stats &operator=(Stats &&) = default;
   Stats &operator=(const Stats &) = default;
   ~Stats();
+  void PrintStats();
 
   float max_health;
   float max_hunger;
@@ -24,5 +26,13 @@ static Stats PlayerStats;
 Stats::Stats()
     : max_health(100), max_hunger(100), max_warmth(100), health(100),
       hunger(100), warmth(100), defense(0), cold_resist(0) {}
+
+void Stats::PrintStats() {
+  std::cout << "Health: " << health << "/" << max_health
+            << "\nHunger: " << hunger << "/" << max_hunger
+            << "\nWarmth: " << warmth << "/" << max_warmth
+            << "\nDefense: " << defense << "\nCold Resist: " << cold_resist
+            << std::endl;
+}
 
 Stats::~Stats() {}
